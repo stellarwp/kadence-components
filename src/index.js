dc879207b5b5b5d3089595377786a86ac4d03349
Mark Woodard <mark.c.woodard@gmail.com>
2024-05-26 02:27:28 -0500

    new ColorControlWithStates component that simplifies the common pattern of color/background with hover/active states

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './editor.scss';
 
 export { default as PopColorControl } from './pop-color-control';
 export { default as SinglePopColorControl } from './single-pop-color-control';
+export { default as ColorControlWithStates } from './color-control-with-states';
 export { default as ColorIcons } from './color-icons';
 export { default as ColorPicker } from './color-picker';
 export { default as ColorFields } from './color-fields';
